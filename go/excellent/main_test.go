d8bfe4517783ee7583d0d273db23b14b788d14c1
Awtanabe <panser098@gmail.com>
2025-01-25 23:52:56 +0900

    dd

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -11,6 +11,7 @@ func TestEventOrOdd(t *testing.T) {
 	result := EventOrOdd(10)
 
 	log.Print("hoge")
+	log.Print("hoge")
 	if result != "event" {
 		t.Errorf("expected event actual %s", result)
 	}
